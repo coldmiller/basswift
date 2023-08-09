71234230139bf8188496a13d82af3577868030a7
cole miller <122376256+coldmiller@users.noreply.github.com>
2023-08-09 08:35:37 -0500

    Initial Commit

---
+++ basswift/basswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  basswiftApp.swift
+//  basswift
+//
+//  Created by Cole Miller on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct basswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
